53efc04bc65b39e5b3e39d372b722f3d5a7f7d43
syamsulbahri613 <syamsulbahri21102@gmail.com>
2022-06-22 18:20:49 +0700

    change navbar product and add image

--- application/views/content/product/products.php
+++ application/views/content/product/products.php
@@ -7,34 +7,38 @@
         </div>
     </div>
 </div>
-    <div class="costumproduct">
-        <a href="<?= base_url() ?>product/productsById/5">BATA EXPOSE</a>
-        <a href="<?= base_url() ?>product/productsById/6">BATA TEMPEL</a>
-        <div class="drop">
-            <button class="dropbtn">ROSTER
-            </button>
-            <div class="drop-content">
-                <a href="<?= base_url() ?>product/productsById/1">ROSTER WHITE</a>
-                <a href="<?= base_url() ?>product/productsById/2">ROSTER RED</a>
-                <a href="<?= base_url() ?>product/productsById/3">ROSTER SEMEN</a>
-                <a href="<?= base_url() ?>product/productsById/4">ROSTER TANAH LIAT</a>
+<div class="costumproduct">
+    <a href="<?= base_url() ?>product/productsById/5">BATA EXPOSE</a>
+    <a href="<?= base_url() ?>product/productsById/6">BATA TEMPEL</a>
+    <div class="drop">
+        <button class="dropbtn">ROSTER
+        </button>
+        <div class="drop-content">
+            <a href="<?= base_url() ?>product/productsById/1">ROSTER WHITE</a>
+            <a href="<?= base_url() ?>product/productsById/2">ROSTER RED</a>
+            <a href="<?= base_url() ?>product/productsById/3">ROSTER SEMEN</a>
+            <a href="<?= base_url() ?>product/productsById/4">ROSTER TANAH LIAT</a>
+        </div>
+    </div>
+    <a href="<?= base_url() ?>/product/productsById/10">PAVING</a>
+</div>
+<div class="container-fluid" style="background-color: #fefdf9;">
+    <div class="container py-5" style="background-color: #fefdf9;">
+        <div class="input-group mb-5" style="max-width: 600px; margin: auto;">
+            <input type="text" class="form-control" id="text-produk" placeholder="Cari nama produk..." aria-label="Recipient's username" aria-describedby="button-addon2">
+            <button class="btn btn-outline-secondary" type="button" onclick="cariProduk()" id="button-addon2"><i class="fas fa-search icon"></i></button>
+        </div>
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-2" id="data-item" data-index="0" style="background-color: #fefdf9;">
+        </div>
+        <div class="text-center loading" style="display: none">
+            <div class="fa-5x">
+                <i class="fas fa-spinner fa-pulse"></i>
             </div>
         </div>
-        <a href="<?= base_url() ?>/product/productsById/10">PAVING</a>
-    </div>
-    <div class="container-fluid" style="background-color: #fefdf9;">
-        <div class="container py-5" style="background-color: #fefdf9;">
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-2" id="data-item" data-index="0" style="background-color: #fefdf9;">
-            </div>
-            <div class="text-center loading" style="display: none">
-                <div class="fa-5x">
-                    <i class="fas fa-spinner fa-pulse"></i>
-                </div>
-            </div>
 
-        </div>
     </div>
-    <!-- <nav class="navbar navbar-expand-lg p-5" style="background-color: #fefdf9;">
+</div>
+<!-- <nav class="navbar navbar-expand-lg p-5" style="background-color: #fefdf9;">
             <div class="collapse navbar-collapse d-flex justify-content-around mt-5" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <a class="nav-link px-4" href="<?= base_url() ?>product/productsById/5">BATA EXPOSE</a>
