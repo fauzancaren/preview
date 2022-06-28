d57f041fd2cdbb247c38161a8bfac8766445ba99
syamsulbahri613 <syamsulbahri21102@gmail.com>
2022-06-28 14:48:08 +0700

    change product

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -63,55 +63,23 @@ class Product extends CI_Controller
                               </div>
 
                                  <ul class="icon-product">
-                                    <li>
+                                    <li class="lip1">
                                        <i class="fas fa-heart"></i>
                                        <span>Add Favorite</span>
                                     </li>
-                                    <li>
+                                    <li class="lip1">
                                        <i class="fas fa-shopping-cart"></i>
                                        <span>Add Cart</span>
-                                    </li>
-                                    <li>
-                                       <i class="fas fa-eye"></i>
-                                       <span>View Detail</span>
                                     </li>
                                  </ul>
                           </div>
                            <small class="align-self-center align-self-lg-start px-2 mt-2 fw-bold" style="color: #896a5e; font-size: 1em; min-height: 46px;">' . $row->MsItemName . '</small>
                            <span class="align-self-center align-self-lg-start px-sm-2 mb-2 fw-normal" style="font-size: 1.3vh;">' . $row->MsItemSize . '</span>
                            <span class="align-self-center align-self-lg-start px-sm-2 fw-normal mb-2" style="font-size: 1.7vh;">Rp. ' . number_format($row->MsItemPrice) . ' /' . $row->MsItemUoM . '</span>
-                           <ul class="d-flex p-0 align-self-center align-self-lg-start px-sm-2 fw-normal mb-2" style="list-style: none; color: orange;">
-                              <li><i class="fas fa-star"></i></li>
-                              <li><i class="fas fa-star"></i></li>
-                              <li><i class="fas fa-star"></i></li>
-                              <li><i class="fas fa-star"></i></li>
-                              <li><i class="fas fa-star"></i></li>
-                           </ul>
                         </div>
                      </div>
                   </a>';
       }
-
-      // foreach ($data as $row) {
-      //    if($delay == 400) { $delay=100 ;}else{ $delay+=100;}
-      //       $html .= '<a data-aos="fade-up" data-aos-delay="'.$delay.'" data-aos-once="false" class ="col" style="text-decoration: none; color: #000000bd;" href="' . base_url("product/detailItem/" . $row->MsItemId) . '">
-      //                   <div class="d-flex flex-column p-md-2" style="background-color: #f2f2f22b;box-shadow: 1px 2px 2px 0px #a3a2a291;     border: 1.2px solid #a3a2a280; font-family: Montserrat, sans-serif; height: 100%;">
-      //                      <div class="d-flex flex-column item p-md-2" style="position: relative;"> 
-      //                         <img class="img-fluid mb-2 align-self-center img-product" src="' . base_url("function/functionimage/product/") . $row->MsItemCode . '" alt="">
-      //                         <small class="align-self-center align-self-lg-start px-2 mt-2 fw-bold" style="color: #896a5e; font-size: 1em; min-height: 46px;">' . $row->MsItemName . '</small>
-      //                         <span class="align-self-center align-self-lg-start px-sm-2 mb-2 fw-normal" style="font-size: 1.3vh;">' . $row->MsItemSize . '</span>
-      //                         <span class="align-self-center align-self-lg-start px-sm-2 fw-normal mb-2" style="font-size: 1.7vh;">Rp. ' . number_format($row->MsItemPrice) . ' /' . $row->MsItemUoM . '</span>
-      //                         <ul class="d-flex align-self-center align-self-lg-start px-sm-2 fw-normal mb-2" style="list-style: none; color: orange;">
-      //                            <li><i class="fas fa-star"></i></li>
-      //                            <li><i class="fas fa-star"></i></li>
-      //                            <li><i class="fas fa-star"></i></li>
-      //                            <li><i class="fas fa-star"></i></li>
-      //                            <li><i class="fas fa-star"></i></li>
-      //                         </ul>
-      //                      </div>
-      //                   </div>
-      //                </a>';
-      //    }
       echo $html;
    }
 }
